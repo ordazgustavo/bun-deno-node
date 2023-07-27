5354081f1a66337117ce99cce0d891664b67b69c
Gustavo Ordaz <ordazsgustavo@gmail.com>
2023-07-27 22:11:02 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,43 +1,29 @@
 import ts from "typescript";
-// const ts = require('typescript');
 
 function compile(fileNames, options) {
   const program = ts.createProgram(fileNames, options);
-  // const emitResult = program.emit();
 
   const allDiagnostics = ts.getPreEmitDiagnostics(program);
-  // .concat(emitResult.diagnostics);
 
   let result = "";
   for (let i = 0; i < allDiagnostics.length; i++) {
     const diagnostic = allDiagnostics[i];
 
-    // allDiagnostics.forEach((diagnostic) => {
     if (diagnostic.file) {
       const { line, character } = ts.getLineAndCharacterOfPosition(
         diagnostic.file,
         diagnostic.start
       );
       const message = diagnostic.messageText;
-      // const message = ts.flattenDiagnosticMessageText(
-      //   diagnostic.messageText,
-      //   "\n"
-      // );
       result = `${result}\n${diagnostic.file.fileName} (${line + 1},${
         character + 1
       }): ${message}`;
     } else {
-      // const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
       const message = diagnostic.messageText;
       result = `${result}\n${message}`;
     }
-    // });
   }
   console.log(result);
-
-  // const exitCode = emitResult.emitSkipped ? 1 : 0;
-  // console.log(`Process exiting with code '${exitCode}'.`);
-  // process.exit(exitCode);
 }
 
 compile(
